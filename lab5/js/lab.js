ece4949c8292c79598c57fb65dde5cc62dcba4eb
sebastian <64922901+check-the-time@users.noreply.github.com>
2020-09-16 09:43:25 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,24 @@
 /*
-  * Author: Sebastian Tamiano
+    * Author: Sebastian Tamiano
   * Partner: Marta Gubanova
   * Created: 15 September 2020
+  * License: Public Domain
 */
 // Define variables
 var make = "Toyota";
 var model = "AE86";
 var color = "White";
-var year = 1983;
+var ownIt = false;
+var year = 1986;
 var currentYear = 2020;
 
 // Calculate age
 age = currentYear - year;
 
-// Output
-document.writeIn("Make: " + make + "<br>");
-document.writeIn("Model: " + model + "<br>");
-document.writeIn("Color: " + color + "<br>");
-document.writeIn("Year: " + year + "<br>");
-document.writeIn("Age: " + age + "<br>");
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
+document.writeln("It is " + ownIt + " that I own it<br>");

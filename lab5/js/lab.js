0d93378f86172151768af70544748e2e8d1016de
sebastian <64922901+check-the-time@users.noreply.github.com>
2020-09-16 09:38:40 -0700

    edit

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -4,11 +4,11 @@
   * Created: 15 September 2020
 */
 // Define variables
-  var make = "Toyota";
-  var model = "AE86";
-  var color = "White";
-  var year = 1983;
-  var currentYear = 2020;
+var make = "Toyota";
+var model = "AE86";
+var color = "White";
+var year = 1983;
+var currentYear = 2020;
 
 // Calculate age
 age = currentYear - year;

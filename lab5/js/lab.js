27289b06dbc4889d4243d4bf6f2c2a5ef131818a
sebastian <64922901+check-the-time@users.noreply.github.com>
2020-09-16 09:00:09 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+//*
+  * Author: Sebastian Tamiano
+  * Partner: Marta Gubanova
+  * Created: 15 September 2020
+  /*
+  // Define variables
+  var make = "Toyota"
+  var model = "AE86"
+  var color = "White"
+  var year = 1983
+  var currentYear = 2020
+
+  // Calculate age
+age = currentYear - year;
+
+// Output
+document.writeIn("Make: " + make + "<br>");
+document.writeIn("Model: " + model + "<br>");
+document.writeIn("Color: " + color + "<br>");
+document.writeIn("Year: " + year + "<br>");
+document.writeIn("Age: " + age + "<br>");

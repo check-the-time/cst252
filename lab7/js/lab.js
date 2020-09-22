5809fbcf9727bc6f2d5f22a862abafb953d8ed6b
sebastian <64922901+check-the-time@users.noreply.github.com>
2020-09-22 11:01:49 -0700

    lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,20 +5,13 @@
  */
 
 
- // Define an array and an object
- var myTransport = ["Car", "Walking", "Bike", "Bus"];
-
- var myMainRide = {
-   make: "Toyota",
-   model: "AE86",
-   year: 1986,
-   color: "White",
-   age: function() {
-     return 2020 - year
-   }
- };
-
- // Output
- document.writeln("This is how I get around: " + myTransport + "<br>");
- document.writeln("My main ride is a car: <pre>",
-     JSON.stringify(myMainRide, null, '\t'), "</pre>");
+ // Function
+function randomizeName(userName) {
+var nameArray = userName.toLowerCase().split("");
+console.log("nameArray =", nameArray);
+var shuffledArray = shuffleArray(nameArray);
+console.log("shuffledArray =", shuffledArray);
+var shuffledString = shuffledArray.join("");
+var newName = toTitleCase(shuffledString);
+return newName;
+ }

ced076287940d0c678edc8c4a45e25b52bac85e9
sebastian <64922901+check-the-time@users.noreply.github.com>
2020-09-24 10:59:18 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Sebastian Tamiano & Marta Gubanova
+ * Created: 22 September
+ * License: Public Domain
+ */
+
+ // Function: accepts a string and sorts it in alphabetical order
+ // Param: string
+ // Returns: sorted, reverse array
+ function sortUserName(userName) {
+ return userName.toLowerCase().split("").sort().join("");
+ }
+ // Function: prompts user input
+ function main() {
+ var userName = window.prompt("ALPHABETIZ-O-RAY! Enter any word to alphabetize it and see the result at the bottom!");
+
+ // Output
+ document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+ var sortedName = sortUserName(userName);
+ document.writeln("ZAP! Here's your alphabetized word: " + "</br>" + sortedName);
+ }
+ // Input
+ main();
